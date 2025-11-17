09bb0703012225714baae1fb920d937ffb370d81
dipashah-farthest <dipa.shah@farthestgate.co.uk>
2025-11-17 10:08:09 +0000

    updated userdata file

---
+++ data/userData.ts
@@ -0,0 +1,9 @@
+export interface userData{
+    email: string,
+  password: string
+}
+
+export const userData : userData = {
+    email: "frant_ofis_test+mcc_dipa2@outlook.com",
+  password: "Dipa123!"
+}
